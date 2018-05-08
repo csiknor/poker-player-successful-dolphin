3a15e6ae057211aff0bfb0d1c66d73cd19b3c569
Cristian Calbeaza <ccalbeaza@endava.com>
2018-05-08 13:03:00 +0300

    Formula

--- src/main/java/org/leanpoker/player/PlayerStrategy.java
+++ src/main/java/org/leanpoker/player/PlayerStrategy.java
@@ -7,6 +7,8 @@ public class PlayerStrategy {
     private int playerStack;
     private int playerBet;
     private Card[] playerCards;
+    private float player_aggression_level = 0.4f;
+    private int small_blind;
 
     public PlayerStrategy(int current_buy_in, int minimum_raise, int playerStack, int playerBet, Card[] playerCards) {
         this.current_buy_in = current_buy_in;
@@ -14,12 +16,13 @@ public class PlayerStrategy {
         this.playerStack = playerStack;
         this.playerBet = playerBet;
         this.playerCards = playerCards;
+        this.small_blind = 0;
     }
 
     public int executePlay() {
 
         if (havePair() || hasHighCard(playerCards[0]) || hasHighCard(playerCards[1])) {
-            return minimum_raise;
+            return minimum_raise + raiseAmount();
         }
 
         return 0;
@@ -36,5 +39,12 @@ public class PlayerStrategy {
     private boolean hasHighCard(Card card) {
 
         return card.getRank().matches("[A-Z]");
+    }
+
+    private int raiseAmount() {
+        int hand_rank = 0;
+        int hand_value = 0;
+        int hand_second_value = 0;
+        return Math.round(small_blind * hand_rank * hand_rank * (1 + hand_value/14) * (1 + hand_second_value/28) * player_aggression_level);
     }
 }
